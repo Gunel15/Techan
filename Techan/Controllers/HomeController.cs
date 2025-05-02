d5c6eac6f350bb418f81368a3568cc519455fbb2
unknown <you@example.com>
2025-05-01 19:49:48 -0700

    Front-to back and as

---
+++ Techan/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Techan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+    }
+}
