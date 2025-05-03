db1ede6fb3f199e6eaaabaa61503f05f53a3f20f
unknown <you@example.com>
2025-05-03 13:34:55 -0700

    Admin panel is updated

--- Techan/Areas/Admin/Controllers/DashboardController.cs
+++ Techan/Areas/Admin/Controllers/DashboardController.cs
@@ -2,8 +2,10 @@
 
 namespace Techan.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class DashboardController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
--- Techan/Program.cs
+++ Techan/Program.cs
@@ -27,7 +27,7 @@ namespace Techan
             app.UseAuthorization();
             app.MapControllerRoute(
             name: "areas",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
