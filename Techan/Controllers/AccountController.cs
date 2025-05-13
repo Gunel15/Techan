f7b07ba97fb64a37a39223ae7ad85b8e11ceef4c
unknown <you@example.com>
2025-05-13 15:08:23 -0700

    Roles assigned

--- Techan/Controllers/AccountController.cs
+++ Techan/Controllers/AccountController.cs
@@ -7,7 +7,7 @@ using Techan.ViewModels.Account;
 
 namespace Techan.Controllers
 {
-    public class AccountController(UserManager<User> _userManager, SignInManager<User> _signInManager,RoleManager<Role> _roleManager) : Controller
+    public class AccountController(UserManager<User> _userManager, SignInManager<User> _signInManager, RoleManager<Role> _roleManager) : Controller
     {
         public async Task<IActionResult> Register()
         {
@@ -32,6 +32,16 @@ namespace Techan.Controllers
                 {
                     ModelState.AddModelError("", error.Description);
                 }
+                return View();
+            }
+            var roleResult = await _userManager.AddToRoleAsync(user, nameof(Roles.User));
+            if (!roleResult.Succeeded)
+            {
+                foreach (var error in roleResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                await _userManager.DeleteAsync(user);
                 return View();
             }
             return RedirectToAction(nameof(Login));
@@ -66,8 +76,8 @@ namespace Techan.Controllers
             //}
             //await _signInManager.SignInAsync(user, vm.RememberMe);
 
-            var result= await _signInManager.PasswordSignInAsync(user,vm.Password,vm.RememberMe,true);
-            if(!result.Succeeded)
+            var result = await _signInManager.PasswordSignInAsync(user, vm.Password, vm.RememberMe, true);
+            if (!result.Succeeded)
             {
                 if (result.IsLockedOut)
                 {
@@ -80,24 +90,61 @@ namespace Techan.Controllers
                 }
                 else
                     ModelState.AddModelError("", "Username or password is incorrect");
-                    return View();
+                return View();
             }
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index","Home");    
+            return RedirectToAction("Index", "Home");
         }
+
+        //public async Task SeedDatas()
+        //{
+        //    if (await _roleManager.Roles.AnyAsync())
+        //        return;
+        //    foreach(var role in Enum.GetValues(typeof(Roles)))
+        //    {
+        //        await _roleManager.CreateAsync(new Role(role.ToString()));
+        //    }
+        //}
 
         public async Task SeedDatas()
         {
             if (await _roleManager.Roles.AnyAsync())
-                return;
-            foreach(var role in Enum.GetValues(typeof(Roles)))
             {
-                await _roleManager.CreateAsync(new Role(role.ToString()));
+
+                foreach (var role in Enum.GetValues(typeof(Roles)))
+                {
+                    await _roleManager.CreateAsync(new Role(role.ToString()));
+                }
+
+            }
+
+            if (!await _userManager.Users.AnyAsync(x => x.NormalizedUserName == "SUPERADMIN"))
+            {
+               var user=new Models.User
+                {
+                    UserName = "superadmin",
+                    Email = "superadmin@gmail.com",
+                    Fullname = "Superadmin"
+                };
+                await _userManager.CreateAsync(user, "superadmin123");
+                await _userManager.AddToRoleAsync(user, nameof(Roles.SuperAdmin));
+            }
+
+            if (!await _userManager.Users.AnyAsync(x => x.NormalizedUserName == "ADMIN"))
+            {
+                var user = new Models.User
+                {
+                    UserName = "admin",
+                    Email = "admin@gmail.com",
+                    Fullname = "admin"
+                };
+                await _userManager.CreateAsync(user, "admin123");
+                await _userManager.AddToRoleAsync(user, nameof(Roles.Admin));
             }
         }
     }
